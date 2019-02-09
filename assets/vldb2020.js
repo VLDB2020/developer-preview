8903f80435f85a4fe9ded26401930239cac25d09
Shohei Yokoyama <domain_github.com@black.cs.inf.shizuoka.ac.jp>
2019-02-09 20:50:10 +0900

    nav menu (2nd level)

--- assets/vldb2020.js
+++ assets/vldb2020.js
@@ -1,14 +1,3 @@
 const menuActive = (category) => {
-    let chk = document.getElementsByClassName("category-check");
-    for (var c = 0; c < chk.length; c++) {
-        chk[c].checked = false;
-    }
     document.getElementById(category).checked = true;
 }
-
-document.onkeydown = (e) => {
-    e = e || window.event;
-    if (e.keyCode == '40') {
-        document.getElementById("toppage-toggle").checked = !document.getElementById("toppage-toggle").checked ;
-    }
-};
