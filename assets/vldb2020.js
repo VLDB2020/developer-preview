9b0035cf652ff648de964a43ef34c9a3efb22287
Shohei Yokoyama <1260486+abarth500@users.noreply.github.com>
2019-02-11 14:01:23 +0900

    jekyll (#4)

--- assets/vldb2020.js
+++ assets/vldb2020.js
@@ -1,3 +1,6 @@
 const menuActive = (category) => {
-    document.getElementById(category).checked = true;
+    console.log(category);
+    document.getElementById('toppage-cnt').setAttribute('class', category);
+    console.log(document.querySelector('input[name="submenu-active"][value="' + category + '"]'));
+    document.querySelector('input[name="category-active"][value="' + category + '"]').checked = true;
 }
